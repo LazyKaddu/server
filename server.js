39e26e458fa19f4796c3fce994043bbf3be185ff
Aashish Negi <aashishnegi0000@gmail.com>
2024-11-21 19:05:59 +0530

    changes port setting

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ io.on('connection', (socket) => {
     });
 });
 
-// Start the server
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+const PORT = process.env.PORT || 3000; // Fallback to 3000 for local development
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
