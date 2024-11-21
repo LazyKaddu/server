867c1bcbbc01bc24be90ab7cf3aef7c853b86516
Aashish Negi <aashishnegi0000@gmail.com>
2024-11-21 18:53:21 +0530

    aaaah cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const socketIo = require('socket.io');
 const server = http.createServer();
 const io = socketIo(server, {
     cors: {
-        origin: ["http://localhost:5173","http://localhost:3000", "*"], // Allow requests from these origins
+        origin: ["http://localhost:5173","http://localhost:3000"], // Allow requests from these origins
         methods: ["GET", "POST"]
     }
 });
